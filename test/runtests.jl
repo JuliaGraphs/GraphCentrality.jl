c56116eebe69f8e9f6d09912cc52e2698b0becad
Seth Bromberger <github@bromberger.com>
2015-01-09 12:18:23 -0800

    Coveralls

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,14 @@
 using Centrality
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+tests = [
+    "measures/betweenness",
+    "measures/closeness",
+]
 
-# betweeenness centrality - values taken from networkx
-g = Centrality.readgraph("testdata/graph-50-500.csv")
-c = Centrality.readcentrality("testdata/graph-50-500-c.txt")
-z = betweenness_centrality(g)
-@test float32(z) == float32(c)
+
+for t in tests
+    tp = joinpath(Pkg.dir("Centrality"),"test","$(t).jl")
+    println("running $(tp) ...")
+    include(tp)
+end
