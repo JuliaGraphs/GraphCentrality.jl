9d49616cc4782b7a4cfee7a3c1c9e4c5f2e9ee05
Seth Bromberger <github@bromberger.com>
2015-01-09 16:39:33 -0800

    more tests

--- test/measures/betweenness.jl
+++ test/measures/betweenness.jl
@@ -1,5 +1,14 @@
 # betweeenness centrality - values taken from networkx
 g = Centrality.readgraph("testdata/graph-50-500.csv")
+h = graph(["a","b","c","d"],[], is_directed=false)
+add_edge!(h,"a","b"); add_edge!(h,"b","c"); add_edge!(h,"a","c"); add_edge!(h,"c","d")
+
 c = Centrality.readcentrality("testdata/graph-50-500-bc.txt")
 z = betweenness_centrality(g)
 @test float32(z) == float32(c)
+y = betweenness_centrality(g, endpoints=true, normalize=false)
+@test y[1:3] == [122.10760591498584, 159.0072453120582, 176.39547945994505]
+x = betweenness_centrality(g,3)
+@test length(x) == 50
+w = betweenness_centrality(h, normalize=false)
+@test w == [ 0.0, 0.0, 2.0, 0.0 ]
--- test/measures/closeness.jl
+++ test/measures/closeness.jl
@@ -1,5 +1,6 @@
-using Graphs
 h = graph(["a","b","c","d"],[])
 add_edge!(h,"a","b"); add_edge!(h,"b","c"); add_edge!(h,"a","c"); add_edge!(h,"c","d")
 z = closeness_centrality(h, normalize=false)
 @test z == [0.75, 0.6666666666666666, 1.0, 0.0]
+y = closeness_centrality(h)
+@test y == [0.75, 0.4444444444444444, 0.3333333333333333, 0.0]
